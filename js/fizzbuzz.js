7568104c4d2d0a9a266417acf57012d187a90c96
Sarah Adewale <saraphynaadewale@gmail.com>
2022-03-24 18:39:01 +0100

    added fizzBuzz challenge

---
+++ js/fizzbuzz.js
@@ -0,0 +1,18 @@
+// Create a for loop that iterates up to 100 while outputting "fizz" at multiples of 
+// 3, "buzz" at multiples of 5 and "fizzbuzz" at multiples of 3 and 5.
+
+// function fizzBuzz(n){
+    for(let i = 0; i<=100; i++){
+        if(i % 3 === 0 && i % 5 === 0){
+            console.log('fizbuzz') 
+        }else if(i % 3 === 0){
+            console.log('fizz') 
+        }else if(i % 5 === 0){
+            console.log('buzz') 
+        }else{
+            console.log(i)
+        }
+    }
+
+
+// console.log(fizzBuzz(100))
