a54e21e6f8292f97eca2df699ae03b74c9ec2ce4
Sarah Adewale <saraphynaadewale@gmail.com>
2022-04-09 19:30:08 +0100

    added remove exclamaton marks codewars challenge

---
+++ js/remove-exclamation-marks.js
@@ -0,0 +1,12 @@
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given 
+// string.
+
+function removeExclamationMarks(s) {
+   return s.split('').filter((item) => item !== '!').join('')
+}
+
+function removeExclamationMarks(s) {
+  return s.split('!').join('');
+}
+
+console.log(removeExclamationMarks('hello world!'))
