8c78767a2f19ed00b1dfcae639885752ff46be09
Sarah Adewale <saraphynaadewale@gmail.com>
2022-04-14 22:57:45 +0100

    modified combat function codewars

--- js/comabt-funtion.js
+++ js/comabt-funtion.js
@@ -5,6 +5,13 @@ function combat(health, damage) {
     return health > damage ? health - damage : 0
 }
 
+function combat(health, damage) {
+    if(health > damage){
+        return health - damage
+    }else{
+        return 0
+    }
+}
 console.log(combat(100, 5))
 console.log(combat(92, 8))
 console.log(combat(20, 30))
