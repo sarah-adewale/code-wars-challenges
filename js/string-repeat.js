928034908e043f246b77b785ed71b44c5d48cdc3
Sarah Adewale <saraphynaadewale@gmail.com>
2022-05-06 16:51:31 +0100

    added string-repeat codewars challenge

---
+++ js/string-repeat.js
@@ -0,0 +1,16 @@
+// Write a function called repeatStr which repeats the given string string exactly n times.
+
+// repeatStr(6, "I") // "IIIIII"
+// repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+
+function repeatStr (n, s) {
+    let sum = ''
+    for(let i = 0; i < n; i++){
+         sum += s
+    }
+    return sum
+  
+}
+
+console.log(repeatStr(3, "*"))
+console.log(repeatStr(5, "#"))
