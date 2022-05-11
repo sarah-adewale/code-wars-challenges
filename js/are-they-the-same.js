3199efcb7dd7ded9e42a9585f27dad2a2f876273
Sarah Adewale <saraphynaadewale@gmail.com>
2022-05-11 16:01:35 +0100

    added are they the same codewars challenge

--- js/are-they-the-same.js
+++ js/are-they-the-same.js
@@ -1,27 +1,7 @@
-function comp(array1, array2){
-    sortArray1 = array1.sort((a,z) => a - z)
-    sortArray2 = array2.sort((a,z) => a - z)
-    let eachArr = 0
-    let eachArr2 = 0
-    // for(let i = 0; i <=array1; i++){
-    //     let eachArr = array1[i]
-    //     return eachArr
-    // }
-    // for(let i = 0; i <=array2; i++){
-    //     let eachArr2 = array2[i]
-    //     return eachArr2
-    // }
-    sortArray1.forEach(element => {
-        let eachArr = element
-        return eachArr
-    });
-        sortArray2.forEach(element => {
-        let eachArr2 = element
-        return eachArr2
-    });
-
-//   return Math.pow(eachArr, 2) === eachArr2 ? true : false
-
+function comp(array1, array2) {
+  if(array1 == null || array2 == null) return false;
+  array1.sort((a, b) => a - b); array2.sort((a, b) => a - b);
+  return array1.map(v => v * v).every((v, i) => v == array2[i]);
 }
 
 console.log(comp([121, 144, 19, 161, 19, 144, 19, 11], [121, 14641, 20736, 361, 25921, 361, 20736, 361]))
