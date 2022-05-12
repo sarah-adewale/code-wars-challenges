17b784a9c441f10f577b99a5cb467de3569dc365
Sarah Adewale <saraphynaadewale@gmail.com>
2022-05-12 17:08:18 +0100

    added find max and min value codewars challenge

---
+++ js/find-max-and-min-value-of-list.js
@@ -0,0 +1,12 @@
+let min = function(list){
+   
+    return Math.min(...list);
+}
+
+let max = function(list){
+   
+    return Math.max(...list);
+}
+
+console.log(min([4,6,2,1,9,63,-134,566]))
+console.log(max([4,6,2,1,9,63,-134,566]))
