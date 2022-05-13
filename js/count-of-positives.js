9a2efff543db650bc8a4a5a57d07c6d8ef85a051
Sarah Adewale <saraphynaadewale@gmail.com>
2022-05-13 13:20:30 +0100

    refactored count of positives code challenge, modified some files

--- js/count-of-positives.js
+++ js/count-of-positives.js
@@ -21,7 +21,7 @@
 //create a function
 
 function countPositivesSumNegatives(input) {
-    if(input === null || input.length < 1){
+    if(input.length == null || input.length == [ ]){
             return []
         }
     let newArray = []
