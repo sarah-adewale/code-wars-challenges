62eda1852876e6285a78639f844aa1a80f03735c
Sarah Adewale <saraphynaadewale@gmail.com>
2022-05-25 19:44:55 +0100

    added printing arrays codewars challenge

---
+++ js/printing-array-elements-with-comma-delimiters.js
@@ -0,0 +1,13 @@
+// Input: Array of elements
+
+// ["h","o","l","a"]
+
+// Output: String with comma delimited elements of the array in the same order.
+
+// "h,o,l,a"
+
+function printArray(array){
+  return array.toString() //.join()
+}
+
+console.log(printArray(["h","o","l","a"]))
