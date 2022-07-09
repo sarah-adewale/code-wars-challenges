092550c5384b227cd536d3917eee4c3eac972729
Sarah Adewale <saraphynaadewale@gmail.com>
2022-07-09 12:38:14 +0100

    added maximum length codewars challenge

---
+++ js/maximum-length-difference.js
@@ -0,0 +1,30 @@
+// You are given two arrays a1 and a2 of strings. Each string is composed with letters from a to z. Let x be any string in the first array and y be any string in the second array.
+
+// Find max(abs(length(x) − length(y)))
+
+// If a1 and/or a2 are empty return -1 in each language except in Haskell (F#) where you will return Nothing (None).
+
+// Example:
+// a1 = ["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"]
+// a2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"]
+// mxdiflg(a1, a2) --> 13
+
+// function mxdiflg(a1, a2) {
+//    let joined = [].concat(a1, a2).length
+//     return a1 === [] && a2 === [] ? -1 : joined.toString()
+    
+// }
+
+function mxdiflg(a1, a2) {
+  if(a1.length==0||a2.length==0)return -1;
+  return Math.max(Math.max(...a1.map(x=>x.length))-Math.min(...a2.map(x=>x.length)),Math.max(...a2.map(x=>x.length))-Math.min(...a1.map(x=>x.length)))
+}
+
+function mxdiflg(a1, a2) {
+  if (a1.length === 0 || a2.length === 0) return -1
+  let l1 = a1.map(str => str.length)
+  let l2 = a2.map(str => str.length)
+  return Math.max(Math.max(...l1) - Math.min(...l2), Math.max(...l2) - Math.min(...l1))
+}
+
+console.log(mxdiflg(["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"], ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"]))
