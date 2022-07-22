5dc39e429d259442130a26d4fb77e67493aca4da
Sarah Adewale <saraphynaadewale@gmail.com>
2022-07-22 03:31:51 +0100

    added get the middle codewars challenge

---
+++ js/build-tower.js
@@ -0,0 +1,27 @@
+// Build a pyramid-shaped tower given a positive integer number of floors. A tower block is represented with "*" character.
+
+// For example, a tower with 3 floors looks like this:
+
+// [
+//   "  *  ",
+//   " *** ", 
+//   "*****"
+// ]
+// And a tower with 6 floors looks like this:
+
+// [
+//   "     *     ", 
+//   "    ***    ", 
+//   "   *****   ", 
+//   "  *******  ", 
+//   " ********* ", 
+//   "***********"
+// ]
+
+function towerBuilder(n) {
+
+    return [...Array(n)]
+        .forEach((_, i) => console.log([...Array(++i)].map((_, j) => ++j).join(' ')));
+}
+
+console.log(towerBuilder(5))
--- js/print-errors.js
+++ js/print-errors.js
@@ -41,4 +41,15 @@ function printerError(s) {
     // }
 }
 
+function printerError(s) {
+    // your code
+    var count = 0;
+    for(var i = 0; i < s.length; i++) {
+      if (s[i] > "m") {
+        count++;
+      }
+    }
+    return count+"/"+s.length;
+}
+
 console.log(printerError("aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz"))
