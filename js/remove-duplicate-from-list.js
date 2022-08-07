2700a68a0bc883ce964bffaa4f4a19a372a41673
Sarah Adewale <saraphynaadewale@gmail.com>
2022-08-07 15:27:22 +0100

    added remove duplicate from list

---
+++ js/remove-duplicate-from-list.js
@@ -0,0 +1,12 @@
+// Define a function that removes duplicates from an array of numbers and returns it as a result.
+
+// The order of the sequence has to stay the same.
+
+function distinct(a) {
+  return a.filter((item, index, arr) => arr.indexOf(item) === index)
+}
+
+console.log(distinct([1,2])) // [1,2]
+console.log(distinct([1,2,2])) // [1,2]
+console.log(distinct([1])) // [1]
+console.log(distinct([1,1,2])) // [1,2]
