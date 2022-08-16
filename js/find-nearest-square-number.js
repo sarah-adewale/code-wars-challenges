21415db855fabc82e9dee29a44d7719d68234348
Sarah Adewale <saraphynaadewale@gmail.com>
2022-08-16 22:04:36 +0100

    added find nearest square codewars challenge

---
+++ js/find-nearest-square-number.js
@@ -0,0 +1,9 @@
+// Your task is to find the nearest square number, nearest_sq(n), of a positive 
+// integer n.
+
+function nearestSq(n){
+   return Math.pow(Math.round(Math.sqrt(n)), 2)
+}
+
+console.log(nearestSq(1)) //1
+console.log(nearestSq(2)) //1
