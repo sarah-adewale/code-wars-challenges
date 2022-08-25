aff1849733f1f472cd9374dc9dc5962f0ba2dd0f
Sarah Adewale <saraphynaadewale@gmail.com>
2022-08-25 13:33:33 +0100

    added multiplication table for number codewars challenge

--- js/multiplication-table-for-number.js
+++ js/multiplication-table-for-number.js
@@ -31,4 +31,12 @@ const multiTable = (number) => {
   return table;
 }
 
+const multiTable = (number) => {
+  let table = []
+  for(let i = 1; i <= 10; i++){
+    table.push(`${i} * ${number} = ${i * number}`)
+  }
+  return table.join('\n')
+}
+
 console.log(multiTable(5))
