8528b362506cc5ea8663e3772521e619f5716759
Sarah Adewale <saraphynaadewale@gmail.com>
2022-09-30 03:41:33 +0100

    added return the day codewars challenge

---
+++ js/return-the-day.js
@@ -0,0 +1,39 @@
+// Complete the function which returns the weekday according to the input number:
+
+// 1 returns "Sunday"
+// 2 returns "Monday"
+// 3 returns "Tuesday"
+// 4 returns "Wednesday"
+// 5 returns "Thursday"
+// 6 returns "Friday"
+// 7 returns "Saturday"
+// Otherwise returns "Wrong, please enter a number between 1 and 7"
+
+//number, no funny business
+//string of the day that matches the number
+
+function whatday(num) { 
+   switch(num){
+    case 1:
+        return 'Sunday'
+    case 2:
+        return 'Monday'
+    case 3:
+        return 'Tuesday'
+    case 4:
+        return 'Wednesday'
+    case 5:
+        return 'Thursday'
+    case 6:
+        return 'Friday'
+    case 7:
+        return 'Saturday'
+    default:
+        return 'Wrong, please enter a number between 1 and 7'
+
+   }
+}
+
+console.log(whatday(1), 'Sunday')
+console.log(whatday(6), 'Friday')
+console.log(whatday(10), 'Wrong, please enter a number between 1 and 7')
