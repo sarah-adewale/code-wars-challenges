95a05c0275457acdac2ac97576c1644f88d84e9a
Sarah Adewale <saraphynaadewale@gmail.com>
2022-10-13 14:53:07 +0100

    added for in for of loop codewars challenge

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -14,7 +14,7 @@
     //     }
     // }
 
-    for(leti = 0; i <= 100; i++){
+    for(let i = 0; i <= 100; i++){
         if(i % 3 === 0 && i % 5 === 0){
             return 'fizzBuzz'
         }else if(i % 3 === 0){
