5c60f0552dcb67b8adc6b810d03ac9c0ff5caa1a
Sarah Adewale <saraphynaadewale@gmail.com>
2022-11-09 04:10:14 +0100

    modified collatz conjecture codewars challenge

--- js/collatz-conjecture.js
+++ js/collatz-conjecture.js
@@ -36,5 +36,14 @@ var hotpo = function(n){
     }  
 }
 
+//OR
+var hotpo = function(n, acc = 0) {
+  if (n <= 1) {
+    return acc;
+  } else {
+    return hotpo(n%2==0 ? n/2 : 3*n+1, acc+1);
+  }
+}
+
 console.log(hotpo(1), 0)
 console.log(hotpo(5), 5)
