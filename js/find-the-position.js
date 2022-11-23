2232bc707350a3a268e2e133f69a6f06987f3f9b
Sarah Adewale <saraphynaadewale@gmail.com>
2022-11-23 15:51:45 +0100

    modified find the position codewars challenge

--- js/find-the-position.js
+++ js/find-the-position.js
@@ -11,18 +11,9 @@ function position(letter){
     if (code > 64 && code < 91) result += (code - 64) + " ";
   }
 
-  return result.slice(0, result.length - 1);
+  return `Position of alphabet: ${result.slice(0, result.length - 1)}`
 
 }
 
-// function alphabetPosition(text) {
-//   var result = "";
-//   for (var i = 0; i < text.length; i++) {
-//     var code = text.toUpperCase().charCodeAt(i)
-//     if (code > 64 && code < 91) result += (code - 64) + " ";
-//   }
-
-//   return result.slice(0, result.length - 1);
-// }
 
 console.log(position('a'))
