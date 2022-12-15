60744acbad32c3aca688fa1b1d7e5b331de6196a
Sarah Adewale <saraphynaadewale@gmail.com>
2022-12-15 13:21:40 +0100

    added bin to decimal codewars challenge

---
+++ js/bin-to-decimal.js
@@ -0,0 +1,8 @@
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+
+function binToDec(bin){
+  let digit = parseInt(bin, 2)
+  return digit
+}
+
+console.log(binToDec("10010100"))
