00459059c3269e55a2f278aaf18132842cd20f9f
Sarah Adewale <saraphynaadewale@gmail.com>
2022-12-19 22:19:40 +0100

    added is it a number codewars challenge

---
+++ js/is-it-a-number.js
@@ -0,0 +1,25 @@
+// Given a string s, write a method (function) that will return true if its a valid single integer or floating number or false if its not.
+
+// Valid examples, should return true:
+
+// isDigit("3")
+// isDigit("  3  ")
+// isDigit("-3.23")
+// should return false:
+
+// isDigit("3-4")
+// isDigit("  3   5")
+// isDigit("3 5")
+// isDigit("zero")
+
+function isDigit(s) {
+ return s == parseFloat(s)
+}
+
+console.log(isDigit('3'), true)
+console.log(isDigit(' 3 '), true)
+console.log(isDigit('-3.23'), true)
+console.log(isDigit('3-4'), false)
+console.log(isDigit(' 3 5 '), false)
+console.log(isDigit('3 5'), false)
+console.log(isDigit('zero'), false)
