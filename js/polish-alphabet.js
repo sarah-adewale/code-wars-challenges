0c16b0dbba2a4265a1047baf8ff74f08802db3dd
Sarah Adewale <saraphynaadewale@gmail.com>
2022-12-24 04:02:53 +0100

    added polish alphabet codewars challenge

--- js/polish-alphabet.js
+++ js/polish-alphabet.js
@@ -17,22 +17,36 @@
 
 // "Jędrzej Błądziński"  -->  "Jedrzej Bladzinski"
 
-var polishLetters = {
-    "ą": "a",
-    "ć": "c",
-    "ę": "e",
-    "ł": "l",
-    "ń": "n",
-    "ó": "o",
-    "ś": "s",
-    "ź": "z",
-    "ż": "z",
-};
+// var polishLetters = {
+//     "ą": "a",
+//     "ć": "c",
+//     "ę": "e",
+//     "ł": "l",
+//     "ń": "n",
+//     "ó": "o",
+//     "ś": "s",
+//     "ź": "z",
+//     "ż": "z",
+// };
 
-function correctPolishLetters (string) {
-  return string.split('').map((c) => polishLetters[c] || c).join("");
+// function correctPolishLetters (string) {
+//   return string.split('').map((c) => polishLetters[c] || c).join("");
+// }
+
+//OR
+function correctPolishLetters(string){
+    return string
+    .replace(/ą/g, 'a')
+    .replace(/ć/g, 'c')
+    .replace(/ę/g, 'e')
+    .replace(/ł/g, 'l')
+    .replace(/ń/g, 'n')
+    .replace(/ó/g, 'o')
+    .replace(/ś/g, 's')
+    .replace(/ź/g, 'z')
+    .replace(/ż/g, 'z')
+    
 }
-
 
 console.log(correctPolishLetters('Jędrzej Błądziński'), 'Jedrzej Bladzinski')
 console.log(correctPolishLetters('Jędrzćj Błądziński'), 'Jedrzcj Bladzinski')
