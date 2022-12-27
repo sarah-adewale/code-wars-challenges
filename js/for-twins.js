b17852c0a133dc98d5a66852d4d8d3268afa5ae8
Sarah Adewale <saraphynaadewale@gmail.com>
2022-12-27 22:34:50 +0100

    added for twins codewars challenge

---
+++ js/for-twins.js
@@ -0,0 +1,26 @@
+// This kata series was created for friends of mine who just started to learn programming. Wish you all the best and keep your mind open and sharp!
+
+// Task:
+// Write a function that will accept two parameters: variable and type and check if type of variable is matching type. Return true if types match or false if not.
+
+// Examples:
+// 42, "number"   --> true
+// "42", "number" --> false
+
+function typeValidation(variable, type) {
+  // Your code should be here ;) 
+  const valType = typeof(variable)
+//   const typeType = typeof(type)
+  if(valType === type){
+    return true
+  }else{
+    return false
+  }
+}
+
+//shorter
+
+function typeValidation(variable, type){
+    return typeof(variable) === type
+}
+console.log(typeValidation(44, 'number'))
