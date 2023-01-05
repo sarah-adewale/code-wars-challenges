878c2842cea7910a17ba1a818fcdb4a492f39ceb
Sarah Adewale <saraphynaadewale@gmail.com>
2023-01-05 21:13:52 +0100

    added regex count codewars challenge

---
+++ js/regex-count.js
@@ -0,0 +1,24 @@
+// Your task is simply to count the total number of lowercase letters in a string.
+
+// Examples
+// lowercaseCount("abc"); ===> 3
+
+// lowercaseCount("abcABC123"); ===> 3
+
+// lowercaseCount("abcABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~"); ===> 3
+
+// lowercaseCount(""); ===> 0;
+
+// lowercaseCount("ABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~"); ===> 0
+
+// lowercaseCount("abcdefghijklmnopqrstuvwxyz"); ===> 26
+
+function lowercaseCount(str){
+    //How many?
+  let regex = /[a-z]/g 
+  let newStr = str.match(regex)
+ return newStr.length
+}
+
+console.log(lowercaseCount('abc'), 3)
+console.log(lowercaseCount('ABc'), 1)
